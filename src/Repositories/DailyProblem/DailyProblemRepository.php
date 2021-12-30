a133be51c4bd7dd8bc52c6471caf3d6201ffb2eb
Ahmed Ashraf <ahmed29329@gmail.com>
2021-12-30 01:44:10 +0100

    Apply fixes from StyleCI (#3)

--- src/Repositories/DailyProblem/DailyProblemRepository.php
+++ src/Repositories/DailyProblem/DailyProblemRepository.php
@@ -9,54 +9,53 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DailyProblemRepository implements DailyProblemRepositoryInterface
 {
-	/**
-	 * @var HttpClient
-	 */
-	private $client;
+    /**
+     * @var HttpClient
+     */
+    private $client;
 
-	public function __construct(HttpClientInterface $client)
-	{
-		$this->client = $client;
-	}
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
 
-	public function get(): array
-	{
+    public function get(): array
+    {
+        $response = $this->client->request('POST', 'https://leetcode.com/graphql', [
+            'json' => [
+                'query' => 'query questionOfToday {activeDailyCodingChallengeQuestion {link question { title }}}',
+            ],
+        ]);
 
-		$response = $this->client->request('POST', 'https://leetcode.com/graphql', [
-			'json' => [
-				'query' => 'query questionOfToday {activeDailyCodingChallengeQuestion {link question { title }}}',
-			],
-		]);
+        return $this->hydrate($response->toArray()['data']['activeDailyCodingChallengeQuestion'] ?? null);
+    }
 
-		return $this->hydrate($response->toArray()['data']['activeDailyCodingChallengeQuestion'] ?? null);
-	}
+    /**
+     * @param string $query
+     *
+     * @return array
+     */
+    public function search($query): array
+    {
+        return $this->get();
+    }
 
-	/**
-	 * @param string $query
-	 *
-	 * @return array
-	 */
-	public function search($query): array
-	{
-		return $this->get();
-	}
+    /**
+     * @param array $data
+     *
+     * @return LeetCodeItem[]
+     */
+    private function hydrate(array $data)
+    {
+        if ($data === null) {
+            return [];
+        }
 
-	/**
-	 * @param array $data
-	 *
-	 * @return LeetCodeItem[]
-	 */
-	private function hydrate(array $data)
-	{
-		if($data === null) {
-			return [];
-		}
+        $leetcode = new LeetCodeItem();
+        $leetcode->setName($data['question']['title']);
+        $leetcode->setType(LeetCodeType::PROBLEM_OF_THE_DAY);
+        $leetcode->setSlug($data['link']);
 
-		$leetcode = new LeetCodeItem();
-		$leetcode->setName($data['question']['title']);
-		$leetcode->setType(LeetCodeType::PROBLEM_OF_THE_DAY);
-		$leetcode->setSlug($data['link']);
-
-		return [$leetcode];
-	}
+        return [$leetcode];
+    }
 }
